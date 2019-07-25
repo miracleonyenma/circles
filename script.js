4a70cded4bca21c3e389b2faad2237b11a87ab5e
miracleonyenma <miracleficient@gmail.com>
2019-07-25 04:47:31 +0100

    update "color"

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ small = document.querySelector(".small"),
 revolveBox  = container.getBoundingClientRect(),
 centerPoint = window.getComputedStyle(container).transformOrigin,
 centers = centerPoint.split(" "),
-centerY = revolveBox.top + parseInt(centers[1]) - window.pageYOffset,
-centerX = revolveBox.left + parseInt(centers[0]) - window.pageXOffset;          
+// centerY = revolveBox.top + parseInt(centers[1]) - window.pageYOffset,
+// centerX = revolveBox.left + parseInt(centers[0]) - window.pageXOffset; 
+
 //If the mathematical and visual center of the element were the same, you could use the following:
-// centerX = revolveBox.left + revolveBox.width/2 - window.pageXOffset,
-// centerY = revolveBox.top + revolveBox.height/2 - window.pageYOffset;
+centerX = revolveBox.left + revolveBox.width/2 - window.pageXOffset,
+centerY = revolveBox.top + revolveBox.height/2 - window.pageYOffset;
 
 //add event listener for the mouse
 window.addEventListener("mousemove", function(e){
